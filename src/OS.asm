e27780ffb583bfdc892683a3dc415925e346085b
xk-rl <bledion.morina@hotmail.com>
2023-12-30 18:36:27 +0100

    added a start message

--- src/OS.asm
+++ src/OS.asm
@@ -42,22 +42,35 @@ main:
 	mov sp, 0x7C00
 
 	;print Hello World
-	mov si, msg_hello	; si register is where the string should be stored
+	mov si, msg_1		; si register is where the string should be stored
 	call out
-
-	mov si, msg_discord
+	mov si, msg_2
 	call out
-
-	mov si, msg_github
+	mov si, msg_3
+	call out
+	mov si, msg_4
+	call out
+	mov si, msg_5
+	call out
+	mov si, msg_6
+	call out
+	mov si, msg_7
+	call out
+	mov si, msg_8
 	call out
 	
 	hlt
 .hlt
 	jmp .hlt
 
-msg_hello: db 'Hello World!', NEXL, 0
-msg_discord: db 'bomba.cat on discord', NEXL, 0
-msg_github: db 'xk-rl on github', NEXL, 0
+msg_1: db ' __________________________', NEXL, 0
+msg_2: db '/                          \', NEXL, 0
+msg_3: db '| Welcome to LBL OS!       |', NEXL, 0
+msg_4: db '| An open source operating |', NEXL, 0
+msg_5: db '| system made by xk-rl,    |', NEXL, 0
+msg_6: db '| Have fun using it.       |', NEXL, 0
+msg_7: db '| _________________________/', NEXL, 0
+msg_8: db '|/', NEXL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
